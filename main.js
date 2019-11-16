d66e748c7b39bae3195ec3823c505ff070863073
Pierce <nathanm0220@gmail.com>
2019-11-16 18:30:19 -0500

    JS ADDED

--- main.js
+++ main.js
@@ -53,18 +53,86 @@ $(document).ready(function () {
     nickiM: 0,
     postM: 0,
     snoopD: 0
-  }
+  };
 
+  const winningOption = {
+    drizzyD: {
+      name: "Drake",
+      description: "You are Drake! You're extremely loyal and sentimental. You ride or die for your city and cry in public about past relationships. You also have a son that you kept hidden from everyone",
+      image: "assets/drake.png"
+    },
+    kanyeW: {
+      name: "Kanye West",
+      description: "You are Kanye West! You're very down to earth and easy going - you don't care what others think about you. You also hang out with Donald Trump",
+      image: "assets/kanye.png"
+    },
+    nickiM: {
+      name: "Nicki Minaj",
+      description: "You are Nicki Minaj! You're extremely influential towards other people and are not afraid to put yourself out there! You are easy to get along with however you're not someone to be messed with! You also assume people can't spell prague.",
+      image: "assets/nickiminaj.png"
+    },
+    postM: {
+      name: "Post Malone",
+      description: "You are Post Malone! You're super laid back and easy to get along with. You are the type to invite everyone over for a BBQ on a nice Saturday afternoon. You drink beer in the morning instead of coffee.",
+      image: "assets/postmalone.png"
+    },
+    snoopD: {
+      name: "Snoop Dogg",
+      description: "You are Snoop Dogg! You're very outgoing and relaxed, you get along with almost everybody. You love to hang out in the sun and prefer to stay at home rather than go out to clubs to have fun.",
+      image: "assets/snoopdogg.png"
+    }
+  };
+
+
+
+  // smooth scroll
+  var scroll = new SmoothScroll('a[href*="#"]', {
+    speed: 1000,
+    speedAsDuration: true
+  });
+
+  
   
   // calling the rapper object list
   // console.log(rapperTally);
 
   
+  // function that finds the answer of questions and tallies them
   $('form').on('submit', function(e){ 
     e.preventDefault();
-    let answer = $(this).find('input:radio:checked').attr('data-rapper');
-    console.log(answer)
+    let result = $(this).find('input:checked')
 
+
+    // targeting the value in the object
+    for (let i = 0; i < result.length; i++) {
+      console.log(result[i].value)
+        rapperTally[result[i].value]++;
+      // const winnerResult = rapperTally.filter((value) => {
+      //   return rapperTally.value <= 1
+      // });
+      // console.log(winnerResult);
+      // let max = Math.max(rapperTally.value);
+      // console.log(max)
+
+      // for (let value in rapperTally){
+      //   console.log(rapperTally.value) 
+      // }
+      
+
+
+      let winner = Object.keys(rapperTally).reduce((a, b) => rapperTally[a] > rapperTally[b] ? a : b);
+      // if (winner === );
+      console.log(`The winner is ${winner}`)  
+
+    }
+
+    // let winnerNumber = Number(rapperTally);
+    // console.log(winnerNumber)
+
+
+
+
+  
 
   })
 });
